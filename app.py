805f8d86a12d90d807108d0114ce230c1e64b1e5
Anthony <anthonyrandomcarey@gmail.com>
2020-06-02 15:48:38 -0400

    Added a function to clean up player info.

--- app.py
+++ app.py
@@ -1,2 +1,28 @@
-if __name__ == '__main__':
-    pass
+# Import player data
+import constants
+
+# Clean up the player information
+def player_info(player):
+    # Create a new empty dictionary
+    player_info = {}
+    # Bring over player name to new list
+    player_info['name'] = player['name']
+    # Bring over player guardians to new list
+    player_info['guardians'] = player['guardians']
+    # Convert height to an interger
+    player_info['height'] = int(player['height'].replace(' inches', ''))
+    # Convert experience to a boolean value
+    if player['experience'] == 'YES':
+        player_info['experience'] = True
+    else:
+        player_info['experience'] = False
+    # Return new player info
+    return player_info
+
+
+# Create a clean_data function
+
+# Create a balance_teams function
+
+
+# Display stats
